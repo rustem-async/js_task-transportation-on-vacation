8228bf639483b1a21f40d7f0cc22440e28f04208
rustem-async <rustemamfoot@gmail.com>
2026-02-20 12:45:43 +0200

    task done

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -4,7 +4,19 @@
  * @return {number}
  */
 function calculateRentalCost(days) {
-  // write code here
+  const RENTAL_COST = 40;
+  const DISCOUNT_3_DAYS = 20;
+  const DISCOUNT_7_DAYS = 50;
+
+  if (days >= 7) {
+    return RENTAL_COST * days - DISCOUNT_7_DAYS;
+  }
+
+  if (days >= 3) {
+    return RENTAL_COST * days - DISCOUNT_3_DAYS;
+  }
+
+  return RENTAL_COST * days;
 }
 
 module.exports = calculateRentalCost;
