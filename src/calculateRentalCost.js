faa6dadb50816ffe2e6649596ae434eca2ce9317
rustem-async <rustemamfoot@gmail.com>
2026-02-20 13:08:00 +0200

    mistakes addressed

--- src/calculateRentalCost.js
+++ src/calculateRentalCost.js
@@ -7,12 +7,14 @@ function calculateRentalCost(days) {
   const RENTAL_COST = 40;
   const DISCOUNT_3_DAYS = 20;
   const DISCOUNT_7_DAYS = 50;
+  const SHORT_TERM_DAYS = 3;
+  const LONG_TERM_DAYS = 7;
 
-  if (days >= 7) {
+  if (days >= LONG_TERM_DAYS) {
     return RENTAL_COST * days - DISCOUNT_7_DAYS;
   }
 
-  if (days >= 3) {
+  if (days >= SHORT_TERM_DAYS) {
     return RENTAL_COST * days - DISCOUNT_3_DAYS;
   }
 
